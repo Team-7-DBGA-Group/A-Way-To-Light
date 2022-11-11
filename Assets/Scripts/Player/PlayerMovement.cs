a9071aed98a76cf3c64d88d319708f25ef401157
Luca Biagioli <79144594+lucabigio@users.noreply.github.com>
2022-11-11 12:19:14 +0100

    Implemented jump system

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -14,26 +14,53 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private Transform CameraTransform = null;
 
+    [Header("Jump settings")]
+    [SerializeField]
+    private Transform groundCheck = null;
+    [SerializeField]
+    private LayerMask groundMask = new LayerMask();
+    [SerializeField]
+    private float _gravityValue = -9.81f;
+    [SerializeField]
+    private float _groundDistance = 0.2f;
+
+    private CharacterController _characterController;
+
     private float _horizontalInput;
     private float _verticalInput;
-    private CharacterController _characterController;
     private float _turnSmoothVelocity;
+
+    private Vector3 _velocity;
+    private bool _isGrounded;
+    private float jumpHeight = 2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
         _characterController = GetComponent<CharacterController>();
+        float correctHeight = _characterController.center.y + _characterController.skinWidth;
+        _characterController.center = new Vector3(0, correctHeight, 0);
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        _isGrounded = Physics.CheckSphere(groundCheck.position, _groundDistance, groundMask);
+
+        if (_isGrounded && _velocity.y < 0)
+            _velocity.y = -2f;
+
         _horizontalInput = Input.GetAxis("Horizontal");
         _verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(_horizontalInput, 0f, _verticalInput).normalized;
 
-        if(direction.magnitude >= 0.1)
+        
+        if (direction.magnitude >= 0.1)
         {
+            
             //Calcolo rotazione player tenendo conto di dove sta guardando la camera
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + CameraTransform.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, TurnSmoothTime);
@@ -43,9 +70,17 @@ public class PlayerMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
             //Movimento del Player
-            //_characterController.Move(direction * angle * Time.deltaTime);
+            if (Input.GetKey(KeyCode.LeftShift))
+                _characterController.Move(moveDir.normalized * PlayerSpeed * Time.deltaTime * PlayerRunMultiplier);
+            else
+                _characterController.Move(moveDir.normalized * PlayerSpeed * Time.deltaTime);
 
-            _characterController.Move(moveDir.normalized * PlayerSpeed * Time.deltaTime);
         }
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+            _velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * _gravityValue);
+
+        _velocity.y += _gravityValue * Time.deltaTime;
+        _characterController.Move(_velocity * Time.deltaTime);
     }
 }
