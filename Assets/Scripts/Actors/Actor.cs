3d5907549cd3472005c44f7bd634d90f7f82a3f6
Cristian <cri.ghiorzi@gmail.com>
2022-11-17 09:54:05 +0100

    Actor minor changes

--- Assets/Scripts/Actors/Actor.cs
+++ Assets/Scripts/Actors/Actor.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public abstract class Actor : MonoBehaviour
 {
-    public int MaxHealth { get { return maxHealth; } private set { maxHealth = value; } }
-    public int CurrentHealth { get; private set; }
+    public int MaxHealth { get { return maxHealth; } protected set { maxHealth = value; } }
+    public int CurrentHealth { get; protected set; }
     
     [Header("Health settings")]
     [SerializeField]
     private int maxHealth = 0;
 
+    public abstract void Die();
 
     public void TakeDamage(int damage)
     {
+        if(damage<=0)
+            return;
         CurrentHealth -= damage;
-        if(CurrentHealth <= 0)
-        {
+        if (CurrentHealth <= 0)
             Die();
-        }
     }
 
     public void ResetHealth()
@@ -28,12 +29,12 @@ public abstract class Actor : MonoBehaviour
 
     public void Heal(int healAmount)
     {
+        if (healAmount <= 0)
+            return;
         CurrentHealth += healAmount;
         if (CurrentHealth > MaxHealth)
             CurrentHealth = MaxHealth;
     }
-
-    public abstract void Die();
 
     private void Start()
     {
