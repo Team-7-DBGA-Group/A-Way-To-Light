7f89a65d4e4b401102e3c112dd10cee573799037
michaellaros <michael_larosa@studenti.naba.it>
2022-11-18 15:33:33 +0100

    Codice standardizzato

--- Assets/Scripts/Interactable/PickableWeapon.cs
+++ Assets/Scripts/Interactable/PickableWeapon.cs
@@ -1,31 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
 
 public class PickableWeapon : MonoBehaviour
 {
-    private GameObject WeaponSpawn;
     [SerializeField]
-    private GameObject _weapon;
+    private GameObject weapon;
 
+    private GameObject _weaponSpawn;
     // Start is called before the first frame update
     public void Start()
     {
-        WeaponSpawn = GameObject.FindGameObjectWithTag("WeaponSlot");
-        
-
+        _weaponSpawn = GameObject.FindGameObjectWithTag("WeaponSlot");
     }
-    public void OnTriggerEnter(Collider collision)
-        
+    public void OnTriggerEnter(Collider collision) 
     {
-
-        
         if (collision.gameObject.CompareTag("Player"))
         {
-            
             Debug.Log("dentro if");
-            Instantiate(_weapon,  WeaponSpawn.transform.position, WeaponSpawn.transform.rotation, WeaponSpawn.transform.parent);
+            Instantiate(weapon,  _weaponSpawn.transform.position, _weaponSpawn.transform.rotation, _weaponSpawn.transform.parent);
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -9,15 +9,4 @@ public class Weapon : MonoBehaviour
     [SerializeField]
     private int durability = 3;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
