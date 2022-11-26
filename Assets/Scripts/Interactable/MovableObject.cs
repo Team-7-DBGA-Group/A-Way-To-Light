a54ae30c0f9d35515e257ef90b6c363b597e5a7a
Cristian <cri.ghiorzi@gmail.com>
2022-11-26 17:32:20 +0100

    Code polish movable object

---
+++ Assets/Scripts/Interactable/MovableObject.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableObject : MonoBehaviour, IInteractable
+{
+    [Header("Object Movement Settings")]
+    [SerializeField]
+    private float objectMoveSpeed;
+    [SerializeField]
+    private List<GameObject> waypoints;
+
+    private int _listIndex = 0;
+    private bool _canMoveMiddle = false;
+
+    private bool _doOnce = false;
+
+    public void Interact()
+    {
+        if (_doOnce)
+            return;
+
+        if (waypoints.Count > 0)
+            _canMoveMiddle = true;
+    }
+
+    public bool MoveObjectEnd(Vector3 endPoint)
+    {
+        float step = objectMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, endPoint, step);
+        float distanceFromEnd = Vector3.Distance(transform.position, endPoint);
+        if (distanceFromEnd <= 0.01f)
+            return true;
+        return false;
+    }
+
+    void Update()
+    {
+        if (_canMoveMiddle)
+        {
+            if (MoveObjectEnd(waypoints[_listIndex].transform.position))
+            {
+                _listIndex++;
+            }
+            if(_listIndex >= waypoints.Count)
+            {
+                _canMoveMiddle = false;
+                _doOnce = true;
+            }
+        }
+    }
+}
