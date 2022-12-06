c6c0d958f07d0eeccc4817908d3d1c5ef2ada4e0
michaellaros <michael_larosa@studenti.naba.it>
2022-12-06 16:43:05 +0100

    start parenting object in boat

---
+++ Assets/Scripts/Interactable/Transport/TransportableObject.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class TransportableObject : MonoBehaviour
+{
+    [SerializeField]
+    private bool _onTransport = false;
+    [SerializeField]
+    private GameObject _player;
+    // Start is called before the first frame update
+    // LUCA TI MOLLO L'OSSO NON RICONOSCE LA COLLISIONE CON LA BARCA, LE HO PROVATE TUTTE
+    private void Update()
+    {
+        if(_onTransport)
+        {
+            _player.transform.SetParent(gameObject.transform);
+        }
+        
+        else
+        {
+            _player.transform.SetParent(null);
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        _onTransport = true;
+        
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        _onTransport = false;
+        
+    }
+
+
+
+}
