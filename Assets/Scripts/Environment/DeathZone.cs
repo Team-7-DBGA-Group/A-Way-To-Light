18e5fa0bcc772bcf964dc3e3c742f57a632dd2c5
michaellaros <michael_larosa@studenti.naba.it>
2022-12-07 11:12:34 +0100

    esposizione variabili di movimento in acqua all'interno del editor per i designer

--- Assets/Scripts/Environment/DeathZone.cs
+++ Assets/Scripts/Environment/DeathZone.cs
@@ -7,6 +7,10 @@ public class DeathZone : MonoBehaviour
     [Header("DeathZone Settings")]
     [SerializeField]
     private float deathDelay = 1.0f;
+    [SerializeField]
+    private float waterPlayerSpeed = 0.6f;
+    [SerializeField]
+    private float waterJumpHeight = 0.4f;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -14,8 +18,8 @@ public class DeathZone : MonoBehaviour
         if(other.gameObject.TryGetComponent(out actor))
         {
             PlayerMovement playerMovement = actor.GetComponent<PlayerMovement>();
-            playerMovement.PlayerSpeed = 0.6f;
-            playerMovement.JumpHeight = 0.4f;
+            playerMovement.PlayerSpeed = waterPlayerSpeed;
+            playerMovement.JumpHeight = waterJumpHeight;
 
             StartCoroutine(COLateDie(actor));
         }
