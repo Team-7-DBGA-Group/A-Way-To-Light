2e2f9522cdf01cec413a8ae07fddd88058445e95
Luca Biagioli <79144594+lucabigio@users.noreply.github.com>
2022-12-11 03:49:51 +0100

    created lampkey script and added to lamp prefab

---
+++ Assets/Scripts/Interactable/Gates/LampKey.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampKey : GateKey
+{
+    protected override void CustomInteraction()
+    {
+
+    }
+}
