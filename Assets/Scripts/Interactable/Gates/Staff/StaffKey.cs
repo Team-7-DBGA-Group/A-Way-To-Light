7543c2f765e036dee48397dcb9cf0eceb875ca61
Luca Biagioli <79144594+lucabigio@users.noreply.github.com>
2023-01-11 18:11:04 +0100

    Interactable Staff (to polish)

--- Assets/Scripts/Interactable/Gates/Staff/StaffKey.cs
+++ Assets/Scripts/Interactable/Gates/Staff/StaffKey.cs
@@ -1,25 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class StaffKey : GateKey
 {
+    [Header("References")]
+    [SerializeField]
+    private GameObject deactivatingObject;
+
     [Header("Materials settings")]
     [SerializeField]
-    private Material OffMaterial;
+    private Material offMaterial;
     [SerializeField]
-    private Material OnMaterial;
+    private Material onMaterial;
 
     private MeshRenderer _meshRenderer;
+    private Material[] _mats;
+
+    protected override void CustomInteraction()
+    {
+        GameObject destroyObject = deactivatingObject;
+        Destroy(destroyObject);
+    }
 
     private void Awake()
     {
         _meshRenderer = GetComponent<MeshRenderer>();
-        _meshRenderer.material = OffMaterial;
     }
 
-    protected override void CustomInteraction()
-    {
-        _meshRenderer.material = OnMaterial;
-    }
 }
