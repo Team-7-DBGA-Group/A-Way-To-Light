c3fdbf567741a0e5ed9f225edc8e29d242d310ec
Luca Biagioli <79144594+lucabigio@users.noreply.github.com>
2023-01-11 16:35:57 +0100

    Updated playerCollision

--- Assets/Scripts/Actors/Player/PlayerCollision.cs
+++ Assets/Scripts/Actors/Player/PlayerCollision.cs
@@ -7,7 +7,12 @@ public class PlayerCollision : MonoBehaviour
     private void OnTriggerStay(Collider other)
     {
         if (other.transform.CompareTag("Transport"))
-            other.transform.gameObject.GetComponent<TransportableObject>().SetTransport(this.gameObject);
+        {
+            if(other.transform.gameObject.GetComponent<TransportableObject>())
+                other.transform.gameObject.GetComponent<TransportableObject>().SetTransport(this.gameObject);
+            if(other.transform.gameObject.GetComponent<AdvancedTransportableObject>())
+                other.transform.gameObject.GetComponent<AdvancedTransportableObject>().SetTransport(this.gameObject);
+        }
     }
 
     private void OnTriggerExit(Collider other)
