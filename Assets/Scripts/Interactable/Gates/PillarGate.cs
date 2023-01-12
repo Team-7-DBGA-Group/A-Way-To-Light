cfb9ad8733174a748a44e06f809ba56940159608
Luca Biagioli <79144594+lucabigio@users.noreply.github.com>
2023-01-12 20:49:07 +0100

    Gates changing materials onOpen

--- Assets/Scripts/Interactable/Gates/PillarGate.cs
+++ Assets/Scripts/Interactable/Gates/PillarGate.cs
@@ -7,9 +7,25 @@ public class PillarGate : Gate
     [Header("References")]
     [SerializeField]
     private Animator animator = null;
+    [SerializeField]
+    private MeshRenderer pillarLightRenderer;
+
+    [Header("Settings")]
+    [SerializeField]
+    private Material closedGateMaterial; 
+    [SerializeField]
+    private Material openGateMaterial;
+
+    private void Start()
+    {
+        if(pillarLightRenderer != null)
+         pillarLightRenderer.material = closedGateMaterial;    
+    }
 
     protected override void GateOpenedAction()
     {
         animator.SetTrigger("Open");
+        if (pillarLightRenderer != null)
+            pillarLightRenderer.material = openGateMaterial;
     }
 }
