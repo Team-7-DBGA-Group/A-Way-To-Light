a2d596ccfaf8c39bd4a6d837cdeae67cd3f71570
Luca Biagioli <79144594+lucabigio@users.noreply.github.com>
2023-01-16 14:12:12 +0100

    CrateTrap destroy on hit

--- Assets/Scripts/Interactable/Traps/CrateTrap.cs
+++ Assets/Scripts/Interactable/Traps/CrateTrap.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class CrateTrap : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField]
+    private GameObject brokenCrate;
+
     private void Awake()
     {
         GetComponent<BoxCollider>().isTrigger = true;
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject crateToDestroy = Instantiate(brokenCrate, transform.position, transform.rotation);
+        crateToDestroy.transform.localScale = transform.localScale;
+
+        Destroy(this.gameObject);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            GetComponent<BoxCollider>().isTrigger = false;
+        if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
             GetComponent<BoxCollider>().isTrigger = false;
     }
 
